de63188158695f74ce76d6ddab11a8d5914db6a8
Cuong Nguyen <nguyencuong6696@gmail.com>
2020-05-04 16:05:47 +0700

    Navigation and custom list issue

---
+++ app/src/main/java/View/ListIssue.java
@@ -0,0 +1,16 @@
+package View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quanlysucotruncu.R;
+
+public class ListIssue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_host);
+    }
+}
---
+++ app/src/main/java/View/NavHost.java
@@ -0,0 +1,16 @@
+package View;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.quanlysucotruncu.R;
+
+public class NavHost extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nav_host);
+    }
+}
